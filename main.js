49e387ac495025f811f039906776b57a43cea756
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-08-05 14:45:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,74 @@
+
+const investment_input = document.getElementById("investment");
+const start_price_input = document.getElementById("start-price");
+const end_price_input = document.getElementById("end-price");
+const buy_fee_input = document.getElementById("buy-fee");
+const sell_fee_input = document.getElementById("sell-fee");
+const min_buy_fee_input = document.getElementById("buy-min-fee");
+const min_sell_fee_input = document.getElementById("sell-min-fee");
+
+const start_button = document.getElementById("button-start");
+const reinvest_button = document.getElementById("button-reinvest");
+
+const result_output = document.getElementById("profit");
+const iteration_counter_output = document.getElementById("iteration-counter");
+const days_counter_output = document.getElementById("days-counter-pdt");
+const weeks_counter_output = document.getElementById("weeks-counter-pdt");
+const months_counter_output = document.getElementById("months-counter-pdt");
+
+let ITERATION_COUNTER = 0
+
+
+
+const percentageOf = (whole, percentage) => (whole / 100) * percentage
+
+const getInputs = () => {
+	return {
+		investment : parseFloat(investment_input.value),
+		startprice : parseFloat(start_price_input.value),
+		endprice : parseFloat(end_price_input.value),
+		buyfee : parseFloat(buy_fee_input.value),
+		sellfee : parseFloat(sell_fee_input.value),
+		minbuyfee : parseFloat(min_buy_fee_input.value),
+		minsellfee : parseFloat(min_sell_fee_input.value)
+	}
+}
+
+function compute(investment, start_price, end_price, buy_fee_pc, min_buy_fee, sell_fee_pc, min_sell_fee) {
+	const postFeeInvestment = investment - Math.max(min_buy_fee, percentageOf(investment, buy_fee_pc))
+	const assets = postFeeInvestment / start_price
+	const preFeeGain = assets * end_price
+	return preFeeGain - Math.max(min_sell_fee, percentageOf(preFeeGain, sell_fee_pc))
+}
+
+function update_metrics() {
+	ITERATION_COUNTER += 1
+	iteration_counter_output.innerText = String(ITERATION_COUNTER)
+	days_counter_output.innerText = String((ITERATION_COUNTER / 3) * 7).split(/[,.]/gm)[0]
+	weeks_counter_output.innerText = String(ITERATION_COUNTER / 3).split(/[,.]/gm)[0]
+	months_counter_output.innerText = '~ ' + String((ITERATION_COUNTER / 3) / 4).split(/[,.]/gm)[0]
+}
+
+function run(reinvest) {
+	const inputs = getInputs()
+	result_output.innerText = compute(
+		inputs.investment,
+		inputs.startprice,
+		inputs.endprice,
+		inputs.buyfee,
+		inputs.minbuyfee,
+		inputs.sellfee,
+		inputs.minsellfee
+	)
+	if (reinvest) {
+		update_metrics()
+	}
+}
+
+
+
+start_button.onclick = run
+reinvest_button.onclick = _ => {
+	investment_input.value = result_output.innerText.split(/[,.]/gm)[0]
+	run(true)
+}
